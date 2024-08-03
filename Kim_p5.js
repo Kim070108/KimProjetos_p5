ea737bd4afe4c375e706e6f910b26aa3ec8a7615
Kim070108 <kimberlylorrainedeoliveiramelo@gmail.com>
2024-08-03 13:08:43 -0300

    Create Kim_p5.js
    adicionando arquivo html.

---
+++ Kim_p5.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background("MistyRose");
+}
+
+function draw() {
+  stroke("Powderblue");
+  fill("Thistle");
+  
+  if (mouseIsPressed){
+   rect(mouseX, mouseY, 20,35) 
+    
+  }
+}
